cabbf93715da1fbe5679dfab7e573ba8f827981e
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-11 15:43:44 +0530

    Refactor: Welcome Message

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome This is a Employee Wage problem");
+        }
+    }
+}
